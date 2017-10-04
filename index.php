420208e49ecf4bab5d9cdf4ebe7658a9829f1bfa
Unknown <wilkinson.doro@gmail.com>
2017-10-04 10:21:28 +0200

    Création index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+	
+
+	<head>
+		<meta charset="UTF-8">
+		<title>1er Pgm</title>
+	</head>
+	
+	<body>
+	
+		<header>
+			Mon entête
+		</header>
+
+		<article>
+			
+			<?php echo 'Hello World !<br>
+			La somme de 1 et 2 fait '. (1+2); ?>
+
+			<ol>
+				<li>Un</li>
+				<li>Deux</li>
+				<li>Trois</li>
+			</ol>
+		
+		</article>
+
+	<footer>
+		Mon footer
+	</footer>
+	
+	</body>
+
+</html>
