972c25da1a1cb9b4be9a5f4d115a64c75e2c7031
Unknown <wilkinson.doro@gmail.com>
2017-10-09 16:09:09 +0200

    Commetaire sur une ligne avec dièse

--- tuto/premier_pgm.php
+++ tuto/premier_pgm.php
@@ -1,8 +1,11 @@
 <?php
 
-echo '<strong> Hello World !</strong> 1234';
+echo '<strong> Hello World !</strong> 1234 </br>';
  
- //Ceci est un premier commentaire sur une ligne
+ // Ceci est un premier commentaire sur une ligne
 
-echo 'Hello World';
+echo 'Hello World ! </br>';
 
+# Ceci est un second commentaire sur une ligne
+
+echo 'Bonjour le monde !';
