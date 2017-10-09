219bf1cc33a647c833500b737d3cfbdc4d82ff45
Unknown <wilkinson.doro@gmail.com>
2017-10-09 17:34:53 +0200

    Ajout commentaire multiligne

--- tuto/premier_pgm.php
+++ tuto/premier_pgm.php
@@ -8,4 +8,13 @@ echo 'Hello World !</br></br>';
 
 # Ceci est un second commentaire sur une ligne
 
-echo 'Bonjour le monde !';
+echo 'Bonjour le monde !</br></br>';
+
+/* 
+  Ce programme a été écrit par Emacs
+
+  Il affiche la chaîne 'Hello Word' ! à l'écran
+*/
+
+  echo 'Je suis une ligne de texte après le commentaire multiligne !';
+  
