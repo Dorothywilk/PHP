87f964d27000d9b79201378ceaa735a25bb8d9fe
COTE <GrCOTE7@GMail.com>
2017-10-09 16:24:48 +0200

    Corr

--- tuto/premier_pgm.php
+++ tuto/premier_pgm.php
@@ -1,10 +1,10 @@
 <?php
 
-echo '<strong> Hello World !</strong> 1234 </br>';
+echo '<strong> Hello World !</strong></br></br>';
  
- // Ceci est un premier commentaire sur une ligne
+// Ceci est un premier commentaire sur une ligne
 
-echo 'Hello World ! </br>';
+echo 'Hello World !</br></br>';
 
 # Ceci est un second commentaire sur une ligne
 
