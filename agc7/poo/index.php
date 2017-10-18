971352b1f34f09f58440d61a15c81b3113a4ed5a
COTE <GrCOTE7@GMail.com>
2017-10-18 22:35:35 +0200

    Méthodes magiques

--- agc7/poo/index.php
+++ agc7/poo/index.php
@@ -3,13 +3,21 @@
 <head>
 	<meta charset="UTF-8">
 	<title>POO</title>
+	<link rel="stylesheet" href="../../css/style.css">
+	<link rel="stylesheet" href="../assets/css/gc7.css">
 </head>
 <body>
 
 	<?php
+
 	// include 'personne.php';
 	
-	include 'vehicule.php';
+	// Classe Mère & classe fille
+	// include 'vehicule.php';
+
+
+	// Classe Mère & classe fille
+	include 'kid.php';
 
 
 
