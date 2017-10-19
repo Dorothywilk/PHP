c03469d47653df5dc8fc77a653e5d2a96c9d7f6d
COTE <GrCOTE7@GMail.com>
2017-10-19 15:29:48 +0200

    Factorisation affichage aff()

--- agc7/poo/humains.php
+++ agc7/poo/humains.php
@@ -2,20 +2,20 @@
 
 include '../class/EtreHumain.php';
 
- 
-$oAlice = new Femme('Alice');
-$oAlice->faireDuSport();
-echo '<br/>';
-$oAlice->seDivertir();
-echo '<br/>';
-echo 'Sexe : ', $oAlice->getSexe();
- 
-echo '<br/><br/>';
- 
-$oBob = new Homme('Bobby');
-$oBob->faireDuSport();
-echo '<br/>';
-$oBob->seDivertir();
-echo '<br/>';
-echo 'Sexe : ', $oBob->getSexe();
 
+$oAlice = new Femme('Alice');
+aff($oAlice);
+
+echo '<br/><br/>';
+
+$oBob = new Homme('Bobby');
+aff($oBob);
+
+function aff($personne) {
+	$personne->faireDuSport();
+	echo '<br/>';
+	$personne->seDivertir();
+	echo '<br/>';
+	echo 'Sexe : ', $personne->getSexe();
+
+}
