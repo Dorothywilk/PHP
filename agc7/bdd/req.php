08f1f4b76113bb7c59775fb518d57600f9703a53
COTE <GrCOTE7@GMail.com>
2017-10-26 02:32:01 +0200

    Add table factures

--- agc7/bdd/req.php
+++ agc7/bdd/req.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace GC7;
-
 
 //$q = "
 //Procedure RecherchePeres (in i integer, inout reponse string)
@@ -22,24 +20,72 @@ namespace GC7;
 
 include( '../class/Req.php' );
 
+$sql = 'select id, name as pseudo, email from users where id in (1,15,16)';
+aff('Users');
+req($sql);
+
+$sql = 'select id, id_user, nom, prenom, date_naissance from clients';
+aff('Clients');
+req($sql);
+
+$sql = 'select id as num, clt_id, date, total from factures';
+aff('Factures');
+req($sql);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function req($sql) {
+$cnx = new \PDO( 'mysql:host=localhost;dbname=laravel;charset=utf8', 'root', '' );
+
+$r = $cnx->query( $sql )
+    ->fetchAll( \PDO::FETCH_OBJ );
+  affR($r);
+}
+
+//$maReq = new Requete( (Array) $req );
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
 // Exemple requête simple
-$req = [
-  'sql' => 'req'
-];
+//$req = [
+//  'sql' => 'req'
+//];
+//
+//$maReq = new Requete( (Array) $req );
+//echo $maReq->sql;
+//affR( $maReq->réponse );
 
-$maReq = new Requete( (Array) $req );
-echo $maReq->sql;
-affR( $maReq->réponse );
-
+//////////////////////////////////////////////////////////////////////////////////////////
 // Exemple requête père
-$req = [
-  'sql' => 'getPere1',
-  'item' => 'Transport'
-  //  'item' => 'Marin'
-];
-$maReq = new Requete( (Array) $req );
-aff('Recherche Père');
-affR( $maReq );
+//$req = [
+//  'sql' => 'getPere1',
+//  'item' => 'Transport'
+//  //  'item' => 'Marin'
+//];
+//$maReq = new Requete( (Array) $req );
+//aff('Recherche Père');
+//affR( $maReq );
 
 
 // Affichage Réel
