bbea6b7db7b4044205997faf22885a4d78c08a18
COTE <GrCOTE7@GMail.com>
2017-10-26 09:20:13 +0200

    Add req pour attribution aléatoire d'un propriétaire

--- agc7/bdd/jointures.php
+++ agc7/bdd/jointures.php
@@ -2,8 +2,7 @@
 namespace GC7;
 ?>
 <div class="jumbotron">
-  <h1>Jointure et sous-requêtes</h1>
-
+  <h3>Jointure et sous-requêtes</h3>
   <p class="lead">
     <i class="fa fa-edit"></i> Pour mises à jour (UPDATE)
   </p>
@@ -18,7 +17,7 @@ $req = function ( $sql ) {
   $rep = $cnx->query( $sql )
     ->fetchAll( \PDO::FETCH_OBJ );
 //  aff($rep);
-  affR( $rep );
+  if ( $rep ) affR( $rep );
 };
 
 $nbr = function ( $table ) {
@@ -26,27 +25,28 @@ $nbr = function ( $table ) {
 
   return $cnx->query( 'select count(*) from ' . $table )->fetch()[ 0 ];
 };
-
-
-echo( '...' );
-$sql = 'select * from pets';
-aff( $sql );
-//affR( [$sql] );
-$req( $sql );
-
+/*
+// Requete qui attribue un propriétaire aléatoire, parfois null
+// Annuler clé étrangère pour l'occasion
+ $sql = 'update animaux set clt_id =
+( select id from users where id = (1 +floor(rand()*21)) limit 1);';
+$req($sql);
+*/
 ?>
+
 <div class="jumbotron">
   <p class="h3-responsive">Les tables de référence</p>
   <?php
-  $sql = 'select id, clt_id, nom, espece, sexe from pets limit 3';
-  aff( 'Pets (Les 3 premiers ' . '/' . $nbr( 'pets' ) . ')' );
+  $sql = 'select id, clt_id, nom, espece, sexe from pets limit 4';
+  aff( 'Pets (Les 4 premiers ' . '/' . $nbr( 'pets' ) . ')' );
   $req( $sql );
 
-  aff( 'Animal (Les 3 premiers ' . '/' . $nbr( 'animals' ) . ')' );
-  $sql = 'select id, nom, espece, sexe, commentaires from animals limit 3';
+  aff( 'Animaux (Les 3 premiers ' . '/' . $nbr( 'animaux' ) . ' qui ont un propriétaire)' );
+  $sql = 'select id, clt_id, nom, espece, sexe, commentaires from animaux where clt_id > ""
+ limit 3';
   $req( $sql );
 
-  aff( 'Animal (Les 3 premiers ' . '/' . $nbr( 'users' ) . ')' );
+  aff( 'Users (Les 3 premiers ' . '/' . $nbr( 'users' ) . ')' );
   $sql = 'select id, name as pseudo, email, role from users where id in (1,15,16)';
   $req( $sql );
   ?>
