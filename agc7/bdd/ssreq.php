50899a45d4223719febb42df3034776efb487f7e
COTE <GrCOTE7@GMail.com>
2017-10-26 05:14:15 +0200

    Sous-requete corrélée

--- agc7/bdd/ssreq.php
+++ agc7/bdd/ssreq.php
@@ -34,10 +34,13 @@ aff($sql);
 $req($sql);
 
 
-echo '<mark>Les 2 premières peuvent être réalisées avec jointures, pas celle ci-dessous)</mark><br>';
 echo('Facture du dernier client (Sous-requête corrélée)');
-$sql = 'select concat("00",id) as num, clt_id, date, total
-from factures where id=(select max(id) from clients)';
+$sql = 'select name as pseudo, email
+from users
+where id in (
+select user_id from clients
+where clients.user_id = users.id
+)';
 aff($sql);
 $req($sql);
 
@@ -50,17 +53,16 @@ $req($sql);
 
 
 
-echo '<hr>';
 ?>
 <div class="jumbotron">
   <p class="h3-responsive">Les 3 tables de référence</p>
 <?php
-aff('Users');
-$sql = 'select id, name as pseudo, email from users where id in (1,15,16)';
+aff('Users (Les 3 premiers)');
+$sql = 'select id, name as pseudo, email, role from users where id in (1,15,16)';
 $req($sql);
 
 aff('Clients');
-$sql = 'select id, id_user, nom, prenom, date_naissance from clients';
+$sql = 'select id, user_id, nom, prenom, date_naissance from clients';
 $req($sql);
 
 aff('Factures');
