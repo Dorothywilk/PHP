8560d51d1d7a96b1397d5bdaa22cc670e9cc5cbe
COTE <GrCOTE7@GMail.com>
2017-10-28 05:33:46 +0200

    Regroupement avec RIGHT JOIN

--- agc7/bdd/regroupement.php
+++ agc7/bdd/regroupement.php
@@ -6,7 +6,7 @@ namespace GC7;
     <h3 class="meaDo">Regroupement</h3>
 
     <p class="mt5">
-      Regroupement des lignes en fonction d'un critère
+      Regroupement de lignes en fonction d'un critère, voire de plusieurs
     </p>
 
     <p class="lead">
@@ -43,10 +43,70 @@ $nbr = function ( $table ) {
   return $cnx->query( 'select count(*) from ' . $table )->fetch()[ 0 ];
 };
 
-$sql = "SELECT COUNT(*) AS nb_races
-FROM Race";
+
+$sql1 = "SELECT (select nom_courant from espece where id=espece_id),
+COUNT(*) AS 'Mâles'  FROM animal
+where sexe='M'
+GROUP BY espece_id";
+aff( $sql1 );
+$req( $sql1 );
+
+
+$sql2 = "SELECT (select nom_courant from espece where id=espece_id),
+COUNT(*) AS 'Nombre'  FROM animal
+GROUP BY espece_id";
+aff( $sql2 );
+$req( $sql2 );
+
+
+$sql = 'SELECT Espece.nom_courant, COUNT(animal.espece_id) AS nb_animaux
+FROM Animal
+  RIGHT JOIN Espece ON Animal.espece_id = Espece.id
+  -- RIGHT puisque la table Espece est à droite.
+GROUP BY nom_courant
+ORDER BY nb_animaux desc;';
 aff( $sql );
 $req( $sql );
+
+
+$sql = "SELECT nom_courant, COUNT(*) as nb_animaux
+FROM Animal
+INNER JOIN Espece ON Espece.id = Animal.espece_id
+GROUP BY nom_courant";
+//aff( $sql );
+//$req( $sql );
+
+
+// Regroupement sur plusieurs critères
+$sql = "SELECT sexe, COUNT(*) as nb_animaux
+FROM Animal
+GROUP BY sexe";
+aff( $sql );
+$req( $sql );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
