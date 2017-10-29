fefd1665a0aefaedb7df7a6ee28acbcfa4351c2d
Doro <wilkinson.doro@gmail.com>
2017-10-30 00:07:10 +0100

    ToDoLi ASV

--- tuto/cours/variablesChiffres.php
+++ tuto/cours/variablesChiffres.php
@@ -1,6 +1,6 @@
 <?php
 
-echo 'Ready';
+echo 'Ready.';
 
 
 
@@ -52,3 +52,4 @@ echo 'Ready';
 // $n=17; // 17 // $diviseur = 3; //3
 //echo '<hr><h2>'.$n.' / '.$diviseur.' = '.(int) ($n / $diviseur).' x '.$diviseur.' + '.($n%$diviseur).'</h2>';
 */
+// TODOLI Mettre Fichier origine ASV
