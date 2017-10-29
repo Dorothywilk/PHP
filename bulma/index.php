9de597c6bf299c534975e4f3af5abe84413653a4
Unknown <wilkinson.doro@gmail.com>
2017-10-29 12:00:24 +0100

    yellowdo

--- bulma/index.php
+++ bulma/index.php
@@ -6,136 +6,140 @@
     
 ?>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bulma</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="../css/bulma.css">
-        <link rel="stylesheet" href="../css/style.css">
-    </head>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bulma</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/bulma.css">
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
 
-    <body>
-        <div class="navDo">
-            <h1><a href="/">Accueil</a> | Titre</h1>
-        </div>
-        <div class="main">
+  <body>
+    <div class="navDo">
+      <h1><a href="/">Accueil</a> | Titre</h1>
+    </div>
+    <div class="main">
 
-            <div class="columns mt5">
+      <div class="columns mt5">
 
-                <div class="column cellDo is-capitalized is-size-4">
+        <div class="column cellDo is-capitalized is-size-4">
 
-                    1. sdjgdsgs
+          1. sdjgdsgs
 
-                    <?php
+          <?php
       echo 'Je suis du PHP : ' . ( 2 + 3 );
       ?>
-                </div>
+        </div>
 
-                <div class="column cellDo is-capitalized is-size-4">
+        <div class="column cellDo is-capitalized is-size-4 yellowdo">
 
-                    2.
+          2.
 
-                    <?php
+          <?php
 
-      echo 'Je ne comprends pas et j\'en suis bien triste. De plus je ne comprends pas les racourcis du CSS : ' . ( 10 + 3 );
+      echo 'Les racourcis du CSS : ' . ( 10 + 3 );
 
       ?>
 
-                </div>
-
-                <div class="column cellDo">
-
-                    3
-
-                </div>
-
-            </div>
-
-            <nav class="breadcrumb" aria-label="breadcrumbs">
-                <ul>
-                    <li><a href="https://bulma.io/documentation/components/breadcrumb/">Breadcrum</a></li>
-                    <li><a href="#">Documentation</a></li>
-                    <li><a href="#">Components</a></li>
-
-                    <li class="is-active"><a href="#" aria-current="page">Breadcrumb</a></li>
-                </ul>
-            </nav>
-            <nav aria-label="Étapes de votre commande">
-                <ol>
-                    <li><a href="…">Informations personnelles</a></li>
-                    <li aria-current="step"><em>Paiement</em></li>
-                    <li>Prévisualisation</li>
-                </ol>
-            </nav>
-            <div class="box">
-                <article class="media">
-                    <div class="media-left">
-                        <figure class="image is-64x64">
-                            <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
-                        </figure>
-                    </div>
-                    <div class="media-content">
-                        <div class="content">
-                            <p>
-                                <strong>Dorothy</strong>
-                                <small>@doro</small>
-                                <small>31m</small> <i class="fa fa-heart bleuDo"></i>
-                                <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-                            </p>
-                        </div>
-                        <nav class="level is-mobile">
-                            <div class="level-left">
-                                <a class="level-item">
-              <span class="icon is-small"><i class="fa fa-reply"></i></span>
-                            </a>
-                                <a class="level-item">
-              <span class="icon is-small"><i class="fa fa-retweet"></i></span>
-            </a>
-                                <a class="level-item">
-              <span class="icon is-small"><i class="fa fa-heart"></i></span>
-            </a>
-                            </div>
-                        </nav>
-                    </div>
-                </article>
-            </div>
-
-            <nav class="level">
-                <div class="level-item has-text-centered">
-                    <div>
-                        <p class="heading"><a href="#"><i class="fa fa-facebook fa-2x"></i>acebook</a></p>
-                        <p class="title">
-                            <?= $tweets; ?>
-                        </p>
-                    </div>
-                </div>
-                <div class="level-item has-text-centered">
-                    <div>
-                        <p class="heading">Following</p>
-                        <p class="title">123</p>
-                    </div>
-                </div>
-                <div class="level-item has-text-centered">
-                    <div>
-                        <p class="heading">Followers</p>
-                        <p class="title">456K</p>
-                    </div>
-                </div>
-                <div class="level-item has-text-centered">
-                    <div>
-                        <p class="heading">Likes</p>
-                        <p class="title">789</p>
-                    </div>
-                </div>
-            </nav>
-            <button class="button is-primary is-large .buttonDo"><i class="fa fa-cab"></i> &nbsp;Go go go !
-  </button>
         </div>
-        <footer class="footerDo">
-            <p><a href="https://bulma.io/documentation/overview/start" target="_blank">Bulma</a> | <a href="http://fontawesome.io/icons" target="_blank">Font AWESOME</a>
+
+        <div class="column cellDo">
+
+          3
+
+        </div>
+
+      </div>
+
+      <nav class="breadcrumb" aria-label="breadcrumbs">
+        <ul>
+          <li><a href="https://bulma.io/documentation/components/breadcrumb/">Breadcrum</a></li>
+          <li><a href="#">Documentation</a></li>
+          <li><a href="#">Components</a></li>
+
+          <li class="is-active"><a href="#" aria-current="page">Breadcrumb</a></li>
+        </ul>
+      </nav>
+
+
+      <nav aria-label="Étapes de votre commande">
+        <ol>
+          <li><a href="…">Informations personnelles</a></li>
+          <li aria-current="step"><em>Paiement</em></li>
+          <li>Prévisualisation</li>
+        </ol>
+      </nav>
+
+
+      <div class="box mt10">
+        <article class=" media ">
+          <div class="media-left ">
+            <figure class="image is-64x64 ">
+              <img src="https://bulma.io/images/placeholders/128x128.png " alt="Image ">
+            </figure>
+          </div>
+          <div class="media-content ">
+            <div class="content ">
+              <p>
+                <strong>Dorothy</strong>
+                <small>@doro</small>
+                <small>31m</small> <i class="fa fa-heart bleuDo "></i>
+                <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+              </p>
+            </div>
+            <nav class="level is-mobile ">
+              <div class="level-left ">
+                <a class="level-item ">
+              <span class="icon is-small "><i class="fa fa-reply "></i></span>
+                            </a>
+                <a class="level-item ">
+              <span class="icon is-small "><i class="fa fa-retweet "></i></span>
+            </a>
+                <a class="level-item ">
+              <span class="icon is-small "><i class="fa fa-heart "></i></span>
+            </a>
+              </div>
+            </nav>
+          </div>
+        </article>
+      </div>
+
+      <nav class="level ">
+        <div class="level-item has-text-centered ">
+          <div>
+            <p class="heading "><a href="# "><i class="fa fa-facebook fa-2x "></i>acebook</a></p>
+            <p class="title ">
+              <?= $tweets; ?>
             </p>
-        </footer>
-    </body>
+          </div>
+        </div>
+        <div class="level-item has-text-centered ">
+          <div>
+            <p class="heading ">Following</p>
+            <p class="title ">123</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered ">
+          <div>
+            <p class="heading ">Followers</p>
+            <p class="title ">456K</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered ">
+          <div>
+            <p class="heading ">Likes</p>
+            <p class="title ">789</p>
+          </div>
+        </div>
+      </nav>
+      <button class="button is-primary is-large .buttonDo "><i class="fa fa-cab "></i> &nbsp;Go go go !
+  </button>
+    </div>
+    <footer class="footerDo ">
+      <p><a href="https://bulma.io/documentation/overview/start " target="_blank ">Bulma</a> | <a href="http://fontawesome.io/icons " target="_blank ">Font AWESOME</a>
+      </p>
+    </footer>
+  </body>
 
 </html>
