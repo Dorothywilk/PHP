2bc75fb5d71d3713c458dc20077c409429f20955
Unknown <wilkinson.doro@gmail.com>
2017-10-30 19:38:47 +0100

    VariablesChiffres

--- tuto/cours/variablesChiffres.php
+++ tuto/cours/variablesChiffres.php
@@ -1,9 +1,30 @@
+<h3>Mon cours sur les Opérations mathématiques sur les variables</h3>
 <?php
 
 $ch = 'Ready.<hr>';
 
 echo $ch;
 
+<?php
+  // Déclaration des variables mathématiques
+  $a = 10;
+  $b = 2;
+  $c = 0;
+  // $c vaut 10+2 = 12
+  $c = $a + $b;
+  // $c vaut 10x2 = 20
+  $c = $a * $b;
+  // $c vaut 10/2 = 5
+  $c = $a / $b;
+  // $c vaut 10-2 = 8
+  $c = $a - $b;
+  // $c vaut le reste de la division de $a par $b soit 0
+  $c = $a % $b;
+  // Incrémentation de $a
+  $a++;
+  // Décrémentation de $b
+  $b--;
+?>
 
 
 
@@ -14,45 +35,6 @@ echo $ch;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// $n=17; // 17 // $diviseur = 3; //3
-//echo '<hr><h2>'.$n.' / '.$diviseur.' = '.(int) ($n / $diviseur).' x '.$diviseur.' + '.($n%$diviseur).'</h2>';
-*/
-// TODOLI Mettre Fichier origine ASV
+  /* // $n=17; // 17 // $diviseur = 3; //3 //echo '
+  <hr>
+  <h2>'.$n.' / '.$diviseur.' = '.(int) ($n / $diviseur).' x '.$diviseur.' + '.($n%$diviseur).'</h2>'; */ // TODOLI Mettre Fichier origine ASV
