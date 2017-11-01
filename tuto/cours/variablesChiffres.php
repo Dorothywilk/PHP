e2868ca24674e3f0235e8dd9d77a6b7e7a687ce3
Doro <wilkinson.doro@gmail.com>
2017-11-01 01:03:35 +0100

    Up ToDoDo

--- tuto/cours/variablesChiffres.php
+++ tuto/cours/variablesChiffres.php
@@ -1,13 +1,16 @@
 <h1>Exemples</h1><br>
 <?php
 
+//ToDoDo J'en suis là !
+
+
 $a = 2;
 $b = 3;
 
 echo '$a = ' . $a . '<br>';
 echo '$b = ' . $b . '<hr>';
   
-if ($a === $b){
+if ( $a === $b ){
   echo '$a est identique à $b';
 }
 else echo '$a n\'est pas identique à $b';
