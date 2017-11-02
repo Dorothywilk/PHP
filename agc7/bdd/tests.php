5875494df5b6400864311c35bacb1a5aadc45fbe
COTE <GrCOTE7@GMail.com>
2017-11-02 19:36:37 +0100

    Add count des commentaires

--- agc7/bdd/tests.php
+++ agc7/bdd/tests.php
@@ -10,6 +10,7 @@ namespace GC7;
   <?php
 
   // Commentaires pour un article (Ici 2)
+  /*
   $sql = "SELECT Commentaire.contenu,
       DATE_FORMAT(Commentaire.date_commentaire, '%d/%m/%Y'),
       Utilisateur.pseudo
@@ -18,31 +19,33 @@ FROM Commentaire
   ON Commentaire.auteur_id = Utilisateur.id
 WHERE Commentaire.article_id = 2
 ORDER BY Commentaire.date_commentaire;";
-//    $req( $sql );
+  */
+  //    $req( $sql );
 
 
   ?>
   <p>Auteur - id de l’auteur = 2</p>
   <?php
-  $sql = "SELECT titre, Resume,
-       DATE_FORMAT(date_publication, '%d/%m/%Y'),
-       utilisateur.pseudo
+  $sql = "SELECT DATE_FORMAT(date_publication, '%d/%m/%Y'),
+       utilisateur.pseudo,
+       titre, Resume,
+       (select count(*) from commentaire where commentaire.article_id=article.id)
 from article
   left join utilisateur
   on utilisateur.id = article.Auteur_id
 order by date_publication desc";
-    $req( $sql );
+  $req( $sql );
 
-//
-//  $sql = "SELECT *
-//
-//from utilisateur";
-//  $req( $sql );
-//
-//  $sql = "SELECT *
-//
-//from article";
-//  $req( $sql );
+  //
+  //  $sql = "SELECT *
+  //
+  //from utilisateur";
+  //  $req( $sql );
+  //
+  //  $sql = "SELECT *
+  //
+  //from article";
+  //  $req( $sql );
 
 
   ?>
