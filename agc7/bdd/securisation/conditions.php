f6fc5604d808f6905661f14ac5c2f221f96f066e
COTE <GrCOTE7@GMail.com>
2017-11-06 02:06:37 +0100

    IF() ELSE()

--- agc7/bdd/securisation/conditions.php
+++ agc7/bdd/securisation/conditions.php
@@ -21,25 +21,92 @@ WHERE Commentaire.article_id = 2
 ORDER BY Commentaire.date_commentaire;";
   */
   //    $req( $sql );
-?>
-<h3>Rappel</h3>
-<?php
+  ?>
+  <h3>Rappel</h3>
+  <?php
 
-$sql = "CALL aujourdhui_demain();";
-$pdo = $req( $sql);
+  $sql = "CALL aujourdhui_demain();";
+  $pdo = $req( $sql );
 
   ?>
   <h3>Simple <code>IF(cond, 1, 0)</code></h3>
   <?php
 
   $sql = "SELECT IF(1=1,'oui','non') as '1 = 1 ?'";
-  $pdo = $req( $sql);
+  $pdo = $req( $sql );
   ?>
 
-  Voir <a
-    href="https://openclassrooms.com/courses/administrez-vos-bases-de-donnees-avec-mysql/structurer-ses-instructions"
+  <h3>IF() dans une procédure</h3>
+
+
+  <?php
+  $pdo = pdo();
+  $sql = "DELIMITER |
+CREATE PROCEDURE est_adopte(IN p_animal_id INT)
+BEGIN
+    DECLARE v_nb INT DEFAULT 0;
+    -- On crée une variable locale
+
+    SELECT COUNT(*) INTO v_nb
+    FROM Adoption
+    WHERE animal_id = p_animal_id;
+    -- On met le nombre de lignes correspondant à l'animal
+    -- dans Adoption dans notre variable locale
+
+    IF v_nb > 0 THEN
+    -- On teste si v_nb est supérieur à 0
+    -- (donc si l'animal a été adopté)
+        SELECT \"J'ai déjà été adopté !\" 'Adopté ?';
+    END IF;
+    -- Et on n'oublie surtout pas le END IF et le ; final
+END |
+DELIMITER ;";
+  affLign( $sql );
+  //  $pdo->query( $sql );
+
+
+  $sql = "CALL est_adopte(3);";
+  $req( $sql, $pdo );
+
+  ?>
+  <h3>IF ELSE</h3>
   <?php
 
+  $sql = "DELIMITER |
+  CREATE PROCEDURE message_sexe(IN p_animal_id INT)
+  BEGIN
+  DECLARE v_sexe VARCHAR(10);
+
+  SELECT sexe INTO v_sexe
+  FROM Animal
+  WHERE id = p_animal_id;
+
+  IF (v_sexe = 'F') THEN      -- Première possibilité
+  SELECT 'Je suis une femelle !' AS sexe;
+  ELSEIF (v_sexe = 'M') THEN  -- Deuxième possibilité
+  SELECT 'Je suis un mâle !' AS sexe;
+  ELSE                        -- Défaut
+  SELECT 'Je suis en plein questionnement existentiel...' AS sexe;
+  END IF;
+  END|
+  DELIMITER ;";
+
+  affLign( $sql );
+  //    $pdo->query( $sql );
+
+
+  $sql = "CALL message_sexe(8);   -- Mâle";
+  $req( $sql, $pdo );
+
+
+  $sql = "CALL message_sexe(6);   -- Femelle";
+  $req( $sql, $pdo );
+
+
+  $sql = "CALL message_sexe(9);   -- Ni l'un ni l'autre";
+  $req( $sql, $pdo );
+
+  //  $pdo = $req( $sql );
   echo str_repeat( '<br>', 28 ); // 28
   ?>
 </div>
