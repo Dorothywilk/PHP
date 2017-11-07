8d260db47a23fa1c9af46b4b581754a73dcb2d52
COTE <GrCOTE7@GMail.com>
2017-11-07 17:01:12 +0100

    Usage des tables temporaires

--- agc7/bdd/securisation/tablesTemporaires.php
+++ agc7/bdd/securisation/tablesTemporaires.php
@@ -4,7 +4,17 @@ namespace GC7;
 <div class="jumbotron">
   <h3 class="meaDo pb10">Tables temporaires</h3>
 
-  <p class="lead">Valables que pour la cession en cours</code></p>
+  <p class="lead">Valables que pour la cession en cours</p>
+
+  <div class="lead">
+    <ul>
+      <li>Ne peuvent pas utiliser de clés étrangères</li>
+      <li>Ne peuvent être référencée 2 fois dans la même session</li>
+    </ul>
+    <p>Elles ne concluent pas une transaction, contrairement aux tables normales (Mais <code>CREATE
+        TEMPORY t_table</code>ne sera pas annulé par l'éventuel<code>ROLLBACK</code>).</p>
+  </div>
+
   <p class="lead"><code>DROP TEMPORARY t_table</code></p>
 </div>
 
@@ -31,6 +41,29 @@ ADD COLUMN date_naissance DATETIME;";
   $pdo->query( $sql );
 
   $sql = "DESCRIBE TMP_Animal;";
+  $req( $sql, $pdo );
+  ?>
+
+  
+  <h3>Rôles principaux</h3>
+
+  <p>Copie conforme de la structure :</p>
+  <?php
+  $sql = "CREATE TABLE IF NOT EXISTS Espece_copy
+LIKE Espece;";
+  affLign( $sql );
+  $pdo->query( $sql );
+  ?>
+  <p>Copie des données que l'on veut</p>
+  <?php
+  $sql = "INSERT INTO Espece_copy
+SELECT * FROM Espece
+WHERE prix < 100;";
+  affLign( $sql );
+
+
+  $sql = "SELECT id, nom_courant, prix
+FROM Espece_copy;";
   $req( $sql, $pdo );
 
 
