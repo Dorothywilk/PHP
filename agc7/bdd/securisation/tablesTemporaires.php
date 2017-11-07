aa49e581685a426ce4a0a2ed1bc49d195fd2bf70
COTE <GrCOTE7@GMail.com>
2017-11-07 15:06:19 +0100

    ALTER une table temp

--- agc7/bdd/securisation/tablesTemporaires.php
+++ agc7/bdd/securisation/tablesTemporaires.php
@@ -4,7 +4,8 @@ namespace GC7;
 <div class="jumbotron">
   <h3 class="meaDo pb10">Tables temporaires</h3>
 
-  <p class="lead">...</p>
+  <p class="lead">Valables que pour la cession en cours</code></p>
+  <p class="lead"><code>DROP TEMPORARY t_table</code></p>
 </div>
 
 <div class="maingc7">
@@ -17,6 +18,15 @@ namespace GC7;
     espece_id INT UNSIGNED,
     sexe CHAR(1)
 );";
+  affLign( $sql );
+  $pdo->query( $sql );
+
+  $sql = "DESCRIBE TMP_Animal;";
+  $req( $sql, $pdo );
+
+  $sql = "-- Pas besoin de spécifier qu'elle est temporaire
+ALTER TABLE TMP_Animal
+ADD COLUMN date_naissance DATETIME;";
   affLign( $sql );
   $pdo->query( $sql );
 
