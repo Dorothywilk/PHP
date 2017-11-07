b795f7448bbcc9d47eef7a0ff6520298fe9377a5
COTE <GrCOTE7@GMail.com>
2017-11-07 12:48:55 +0100

    Vues matérialisées

---
+++ agc7/bdd/securisation/vuesMaterialisées.php
@@ -0,0 +1,23 @@
+<?php
+namespace GC7;
+?>
+<div class="jumbotron">
+  <h3 class="meaDo pb10">Vues matérialisées</h3>
+
+  <p class="lead">...</p>
+</div>
+
+<div class="maingc7">
+  <?php
+
+  $pdo = pdo();
+  $sql = "select 123";
+  //  affLign( $sql );
+  //  $pdo->query( $sql );
+  $req( $sql );
+
+
+  echo str_repeat( '<br>', 28 ); // 28
+  ?>
+
+</div>
