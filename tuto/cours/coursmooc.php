f9f0297a642d50831c1f553c61422ea40fc16a2c
Doro <wilkinson.doro@gmail.com>
2017-11-19 15:42:16 +0100

    Re correction

--- tuto/cours/coursmooc.php
+++ tuto/cours/coursmooc.php
@@ -441,16 +441,7 @@ if ($variable == 23)
 }
 
 
-echo '<hr>';
 
-$variable = 23;
-
-if ($variable == 23)
-{
-?>
-    <strong>Bravo !</strong> Vous avez trouvé le nombre mystère !
-    <?php
-}
 
 
 
