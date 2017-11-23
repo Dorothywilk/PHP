4a762d5662c17bd0478332b59c774f6a2dafd571
COTE <GrCOTE7@GMail.com>
2017-11-23 12:31:29 +0100

    To Fix bug BdD blocage qd tout activé

--- agc7/bdd/req.php
+++ agc7/bdd/req.php
@@ -45,16 +45,16 @@ namespace GC7;
 
 // Sécurisation
 
-include( 'securisation/transactions.php' );
-include( 'securisation/verrous.php' );
-include( 'securisation/requetesPreparees.php' );
-include( 'securisation/procedures.php' );
-include( 'securisation/conditions.php' );
-include( 'securisation/erreurs_curseurs.php' );
-include( 'securisation/triggers.php' );
-include( 'securisation/vues.php' );
-include( 'securisation/tablesTemporaires.php' );
-include( 'securisation/vuesMaterialisées.php' );
+//include( 'securisation/transactions.php' );
+//include( 'securisation/verrous.php' );
+//include( 'securisation/requetesPreparees.php' );
+//include( 'securisation/procedures.php' );
+//include( 'securisation/conditions.php' );
+//include( 'securisation/erreurs_curseurs.php' );
+//include( 'securisation/triggers.php' );
+//include( 'securisation/vues.php' );
+//include( 'securisation/tablesTemporaires.php' );
+//include( 'securisation/vuesMaterialisées.php' );
 //
 //include( 'tests.php' );
 //
