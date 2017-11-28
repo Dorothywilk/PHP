171d3611896b469d802a6a706d9db16ffc1525cf
COTE <GrCOTE7@GMail.com>
2017-11-28 19:18:28 +0100

    Add arbreXu() (Boucle)

--- agc7/xu/ri/xu.sql
+++ agc7/xu/ri/xu.sql
@@ -348,3 +348,47 @@ NO SQL
   END;
 -- ------------
 */
+
+
+CALL arbreXu;
+
+DROP PROCEDURE IF EXISTS `arbreXu`;
+
+-- Exemple de procédure avec boucle
+-- Génère ici l'arbre, le groupe
+CREATE PROCEDURE `arbreXu`(IN _noeud INTEGER UNSIGNED,
+                           IN _pref  CHAR(20))
+DETERMINISTIC
+NO SQL
+  BEGIN
+    DECLARE _id INTEGER DEFAULT 0;
+    DECLARE _lien INTEGER DEFAULT 0;
+    DECLARE _nom VARCHAR(255) DEFAULT '';
+
+    DECLARE _fin INTEGER DEFAULT 1;
+    DECLARE _tab CURSOR FOR SELECT
+                              id,
+                              lien,
+                              nom
+                            FROM `parent`
+                            WHERE lien = _noeud;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET _fin = 0;
+
+    OPEN _tab;
+    FETCH _tab
+    INTO _id, _lien, _nom;
+
+    WHILE (_fin)
+    DO
+      INSERT INTO `travail` (`lib`) VALUES (concat(_pref, '... ', _nom));
+
+      CALL arbre(_id, concat('..', _pref));
+
+      FETCH _tab
+      INTO _id, _lien, _nom;
+    END WHILE;
+
+    CLOSE _tab;
+  END;
+-- ------------
+*/
