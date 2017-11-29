91795591a415ddb90e06b663ef372c20913644be
COTE <GrCOTE7@GMail.com>
2017-11-29 15:00:14 +0100

    add boucleXus()

--- agc7/xu/ri/uuu.sql
+++ agc7/xu/ri/uuu.sql
@@ -1,28 +1,16 @@
-USE aaxu;
 
-DROP TABLE IF EXISTS xus;
-CREATE TABLE xus
-    SELECT
-      uid,
-      uname,
-      (SELECT uid
-       FROM www_boos2013.xoops_users xxu
-       WHERE xxu.uname = xu.parr) AS parr
-    FROM www_boos2013.xoops_users xu
-    LIMIT 1;
+CALL boucleXus;
+-- Simul récursivité / wwwbos2013_xoops_users
+DROP PROCEDURE IF EXISTS `boucleXus`;
 
-INSERT INTO xus (uid, uname, parr)
-SELECT
-  uid,
-  uname,
-  (SELECT uid
-   FROM www_boos2013.xoops_users xxu
-   WHERE xxu.uname = xu.parr) AS parr
-FROM www_boos2013.xoops_users xu
-LIMIT 3,1;
-;
-
-INSERT INTO xus (uid, uname, parr) VALUES (77, 'kkk', 1);
-
-SELECT *
-FROM xus;
+-- Exemple de procédure avec boucle
+-- Génère ici l'arbre, le groupe
+DELIMITER |
+CREATE PROCEDURE `boucleXus`()
+DETERMINISTIC
+NO SQL
+  BEGIN
+    DECLARE _uid, _parr INT;
+    SELECT 'OK21';
+  END|
+DELIMITER ;
