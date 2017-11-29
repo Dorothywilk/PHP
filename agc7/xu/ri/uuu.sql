67d9d6d5ea192cf7ec2a464f5a46e68f9caad26a
COTE <GrCOTE7@GMail.com>
2017-11-29 21:18:56 +0100

    MySQL add function getParr(id)

--- agc7/xu/ri/uuu.sql
+++ agc7/xu/ri/uuu.sql
@@ -9,3 +9,44 @@ DETERMINISTIC
   END;
 
 CALL boucleB();
+
+
+DROP PROCEDURE IF EXISTS aaxu.msg;
+CREATE PROCEDURE aaxu.msg()
+NO SQL
+DETERMINISTIC
+  BEGIN
+    SELECT 'Ok';
+
+  END;
+
+CALL aaxu.msg();
+
+DROP FUNCTION IF EXISTS getParr;
+CREATE FUNCTION getParr(idu INT)
+  RETURNS INT
+DETERMINISTIC
+READS SQL DATA
+  BEGIN
+    SELECT parr into @i
+    FROM b
+    WHERE id = idu;
+    RETURN @i;
+  END;
+
+SELECT getParr(3) AS IdParr;
+
+DROP FUNCTION IF EXISTS getCarre;
+CREATE FUNCTION getCarre(id INT)
+  RETURNS INT
+DETERMINISTIC
+READS SQL DATA
+  BEGIN
+    -- select id asIdParr from b where parr = id;
+    RETURN id * id;
+  END;
+
+SET @i = 25;
+SELECT
+  @i,
+  getParr(@i) AS IdParr;
