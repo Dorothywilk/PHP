f59447f0aec567ae580956c68bd2201957481512
COTE <GrCOTE7@GMail.com>
2017-12-07 11:40:19 +0100

    Récup insertXu()

--- agc7/xu/ri/xu.sql
+++ agc7/xu/ri/xu.sql
@@ -1,165 +1,290 @@
-USE aazt;
+DROP DATABASE IF EXISTS `aaxu`;
+CREATE DATABASE `aaxu`
+  DEFAULT CHARACTER SET `latin1`
+  DEFAULT COLLATE `latin1_general_ci`;
 
--- #################### Préparation des tables ##################
+USE aaxu;
 
+-- #################################################################################################
+--
+--                                       CRÉATION TABLE XU
+--
+-- #################################################################################################
+DROP TABLE IF EXISTS xu;
 
-DROP TABLE IF EXISTS aazt.b1;
-CREATE TABLE aazt.b1
-    SELECT
-      uid   AS id,
-      uname AS pseudo,
-      parr  AS parrain
-    FROM aazt.boosteurori;
-
--- ALTER TABLE `b1` COLLATE = 'utf8_unicode_ci';
-ALTER TABLE `b1`
-CHANGE COLUMN `pseudo` `pseudo` VARCHAR(250) NOT NULL DEFAULT '' COLLATE 'latin1_general_ci'
-AFTER `id`;
-ALTER TABLE `b1`
-CHANGE COLUMN `parrain` `parrain` VARCHAR(250) NOT NULL DEFAULT '' COLLATE 'latin1_general_ci'
-AFTER `pseudo`;
-
-
-DELETE FROM b1
-WHERE id = 1 OR id = 15;
-
-
-DROP TABLE IF EXISTS aazt.b2;
-CREATE TABLE `b2` (
-  `id`      INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `pseudo`  VARCHAR(250)     NOT NULL DEFAULT '' COLLATE 'latin1_general_ci',
-  `parrain` VARCHAR(250)     NULL     DEFAULT '0' COLLATE 'latin1_general_ci',
-  `uid`     INT(10) UNSIGNED NULL     DEFAULT '0',
-  `parr`    INT(10) UNSIGNED NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+CREATE TABLE `xu` (
+  `id`      INT(11) UNSIGNED    NOT NULL AUTO_INCREMENT,
+  `pseudo`  VARCHAR(255)        NOT NULL
+  COLLATE 'latin1_general_ci',
+  `lv`      TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+  `typ`     CHAR(1)             NOT NULL DEFAULT 'A' COLLATE 'latin1_general_ci',
+  `lva`     TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+  `lvp`     TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+  `parrain` VARCHAR(255)        NULL     DEFAULT NULL COLLATE 'latin1_general_ci',
+  `parr`    INT(11) UNSIGNED    NULL     DEFAULT NULL,
+  `bg`      INT(11) UNSIGNED    NOT NULL DEFAULT '0',
+  `bd`      INT(11) UNSIGNED    NOT NULL DEFAULT '0',
+  `pf`      INT(11) UNSIGNED    NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  INDEX `lv` (`lv`),
+  INDEX `typ` (`typ`),
+  INDEX `bg` (`bg`),
+  INDEX `bd` (`bd`),
+  INDEX `pf` (`pf`)
 )
   COLLATE = 'latin1_general_ci'
   ENGINE = InnoDB
-  AUTO_INCREMENT = 1;
+  AUTO_INCREMENT = 1
+    SELECT
+      uid                     AS id,
+      uname                   AS pseudo,
+      lv,
+      typ,
+      lva,
+      lvp,
+      parr                    AS parrain,
+      (SELECT uid
+       FROM www_boos2013.xoops_users
+       WHERE uname = xu.parr) AS parr,
+      '0'                     AS bg,
+      +
+      '0'                     AS bd,
+      '0'                     AS pf
+    FROM www_boos2013.xoops_users xu;
 
-- ALTER TABLE `b2` COLLATE = 'utf8_unicode_ci';
+-- ALTER TABLE aaxu.xu ADD PRIMARY KEY (id);
 
-INSERT INTO aazt.b2 (uid, pseudo)
-VALUES
-  (1, 'Aadminli');
+SELECT *
+FROM xu;
+
+-- #################################################################################################
+--
+--             CRÉATION TABLE xut pour test d'insertion (Départ avec Aadminli uniquement)
+--
+-- #################################################################################################
+CREATE TABLE `aaxu`.`xut` (
+  `id`      INT(11) UNSIGNED    NOT NULL AUTO_INCREMENT,
+  `pseudo`  VARCHAR(255)        NOT NULL
+  COLLATE 'latin1_general_ci',
+  `lv`      TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+  `typ`     CHAR(1)             NOT NULL DEFAULT 'A' COLLATE 'latin1_general_ci',
+  `lva`     TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+  `lvp`     TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+  `parrain` VARCHAR(255)        NULL     DEFAULT NULL COLLATE 'latin1_general_ci',
+  `parr`    INT(11) UNSIGNED    NULL     DEFAULT NULL,
+  `bg`      INT(11) UNSIGNED    NOT NULL DEFAULT '0',
+  `bd`      INT(11) UNSIGNED    NOT NULL DEFAULT '0',
+  `pf`      INT(11) UNSIGNED    NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  INDEX `lv` (`lv`),
+  INDEX `typ` (`typ`),
+  INDEX `bg` (`bg`),
+  INDEX `bd` (`bd`),
+  INDEX `pf` (`pf`)
+)
+  COLLATE 'latin1_general_ci'
+  ENGINE = InnoDB
+  ROW_FORMAT = DYNAMIC
+  AUTO_INCREMENT = 27208;
+
+-- Insertion (par copie) de Aadminli
+INSERT INTO `aaxu`.`xut` (`id`, `pseudo`, `lv`, `typ`, `lva`, `lvp`, `parrain`, `parr`, `bg`, `bd`, `pf`)
+  SELECT
+    `id`,
+    `pseudo`,
+    `lv`,
+    `typ`,
+    `lva`,
+    `lvp`,
+    `parrain`,
+    `parr`,
+    `bg`,
+    `bd`,
+    `pf`
+  FROM `xu`
+  WHERE id < 2;
+
+-- Initialisation Aadminli : Parrain et parr null, bornes G et D (Et pf)
+UPDATE aaxu.xut
+SET parrain = NULL, parr = NULL, bg = 1, bd = 2, pf = 0;
+
+-- Re-initialise Aadminli pour départ tests
+TRUNCATE aaxu.xut;
+INSERT INTO `aaxu`.`xut` (`id`, `pseudo`, `lv`, `typ`, `lva`, `lvp`, `parrain`, `parr`, `bg`, `bd`, `pf`)
+VALUES (1, 'Aadminli', 1, 'P', 0, 0, NULL, NULL, 1, 2, 0);
+SELECT *
+FROM xut;
+
+-- #################################################################################################
+--
+--            PROCÉDURE pour INSERTION et calcul des bornes et profondeur (pf)
+--
+-- #################################################################################################
+-- ToDoLi ajout lock Table qd Opé + activer transaction (Cf. arbre/exemple_proc.sql)
+-- ToDoLi Cf. arbre/exemple_proc.sql pour proc avec boucle
+
+CALL insertXu('GrCOTE7', 1);
+SELECT *
+FROM xut;
+SELECT
+  @pseudoParr,
+  @idParr;
+
+CALL insertXu('Doro', 2);
+CALL insertXu('Jade', 3);
+SELECT *
+FROM xut;
 
 
-/*
-INSERT INTO aazt.b1 (id, pseudo, parrain)
-VALUES
-  (2, 'Grcote7', 'Aadminli'),
-  (3, 'Doro', 'Grcote7'),
-  (4, 'Jade', 'Doro'),
-  (5, 'Micky', 'Jeny'),
-  (7, 'Arnaud', 'Félicien'),
-  (8, 'Mimi', 'Doro'),
-  (9, 'Jeny', 'Jade'),
-  (10, 'Julien', 'Rom1'),
-  (11, 'Jonathan', 'Doro'),
-  (12, 'Félicien', 'Julien'),
-  (13, 'rom1', 'Doro'),
-  (14, 'Greg', 'Jonathan'),
-  (15, 'Fanny', 'Jonathan');
-*/
-/*
-SELECT length(parrain), parrain
-FROM aazt.b1
-WHERE pseudo = 'jlb11';
-
-SELECT length(parr), parr
-FROM aazt.boosteurori
-WHERE uname = 'jlb11';
-*/
-DROP PROCEDURE IF EXISTS boucle_b1;
-CREATE PROCEDURE boucle_b1()
+DROP PROCEDURE insertXu;
+DELIMITER |
+CREATE DEFINER =`root`@`localhost` PROCEDURE `insertXu`(
+  IN `pseudoXu` VARCHAR(255),
+  IN `idRef`    INT
+)
   BEGIN
-    DECLARE v_stop, derIdb1, v_id, v_parr, idEnAtt, i INT DEFAULT 0;
-    DECLARE v_pseudo, v_parrain VARCHAR(255);
+    -- START TRANSACTION;
 
-    SELECT max(id)
-    INTO derIdb1
-    FROM b1;
+    DECLARE pseudoRef VARCHAR(255);
+    DECLARE bgRef, bdRef, pfRef INT(11);
 
-    SET i = 1;
+    -- Réc valeur du parr
+    SELECT
+      pseudo,
+      bg,
+      bd,
+      pf
+    INTO pseudoRef, bgRef, bdRef, pfRef
+    FROM xut
+    WHERE id = idRef;
 
-    WHILE i <= derIdb1 DO -- and v_stop<100
+    -- SET @pseudoParr = pseudoRef;
 
-      SELECT
-        id,
-        pseudo,
-        parrain,
-        (SELECT id
-         FROM b2
-         WHERE pseudo = b1.parrain)
-      INTO v_id, v_pseudo, v_parrain, v_parr
-      FROM b1
-      WHERE id = i;
+    UPDATE xut
+    SET bd = bd + 2
+    WHERE bd >= bdRef;
 
-      SET v_stop = v_stop + 1;
+    UPDATE xut
+    SET bg = bg + 2
+    WHERE bg >= bdRef;
 
-      IF v_id AND v_parr
-      THEN
+    INSERT INTO xut (pseudo, parrain, parr, bg, bd, pf)
+    VALUES (pseudoXu, pseudoRef, idRef, bdRef, (bdRef + 1), (pfRef + 1));
 
-        DELETE FROM b1
-        WHERE id = v_id;
+    -- COMMIT;
+  END |
+DELIMITER ;
 
-        INSERT INTO b2 (pseudo, parr, uid, parrain) VALUES
-          (
-            v_pseudo,
-            v_parr,
-            v_id,
-            v_parrain
-          );
-
-        SELECT min(id)
-        INTO idEnAtt
-        FROM b1
-        WHERE parrain = v_pseudo
-              AND id <= i;
-        /*
-                SELECT
-                  i,
-                  idEnAtt;
-        */
-
-        IF idEnAtt
-        THEN
-
-          SELECT (min(id) - 1)
-          INTO i
-          FROM b1
-          WHERE parrain = v_pseudo;
-
-        END IF;
-
-        SET idEnAtt = 0;
-
-      END IF;
-
-      SET i = i + 1;
-
-      SET v_id = 0;
-      SET v_parr = 0;
-      SET v_pseudo = '';
-      SET v_parrain = '';
+SELECT
+  pseudo AS pseudoRef,
+  bg     AS bgRef,
+  bd     AS bdRef,
+  pf     AS pfRef
+FROM xut
+WHERE id = 1;
 
 
-    END WHILE;
+USE aaxu;
+
+DROP PROCEDURE test_boucle;
+DELIMITER |
+CREATE DEFINER =`root`@`localhost` PROCEDURE `test_boucle`(IN `p_id` INT)
+  BEGIN
+    DECLARE v_pseudoId VARCHAR(255);
+
+    DECLARE curs_xus CURSOR
+    FOR SELECT
+          id,
+          pseudo
+        FROM aaxu.xu
+        WHERE id < p_id;
+
+    OPEN curs_xus;
+
+    LOOP
+      FETCH curs_xus
+      INTO v_pseudoId;
+      SELECT CONCAT(v_pseudoId, ' ', id) AS 'Xu';
+    END LOOP;
+
+    CLOSE curs_xus;
+  END|
+
+CALL test_boucle(10);
+
+/*
+CREATE DEFINER =`root`@`localhost` PROCEDURE `test_condition`(IN `p_ville` VARCHAR(100)) BEGIN
+  DECLARE v_nom, v_prenom VARCHAR(100);
+
+  DECLARE curs_clients CURSOR
+  FOR SELECT
+        nom,
+        prenom
+      FROM Client
+      WHERE ville = p_ville;
+
+  OPEN curs_clients;
+
+  LOOP
+    FETCH curs_clients
+    INTO v_nom, v_prenom;
+    SELECT CONCAT(v_prenom, ' ', v_nom) AS 'Client';
+  END LOOP;
+
+  CLOSE curs_clients;
+  END$$
+
+CREATE DEFINER =`root`@`localhost` PROCEDURE `test_condition2`(IN `p_ville` VARCHAR(100)) BEGIN
+  DECLARE v_nom, v_prenom VARCHAR(100);
+
+  -- On déclare fin comme un BOOLEAN, avec FALSE pour défaut
+  DECLARE fin BOOLEAN DEFAULT FALSE;
+
+  DECLARE curs_clients CURSOR
+  FOR SELECT
+        nom,
+        prenom
+      FROM Client
+      WHERE ville = p_ville;
+
+  -- On utilise TRUE au lieu de 1
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET fin = TRUE;
+
+  OPEN curs_clients;
+
+  loop_curseur: LOOP
+    FETCH curs_clients
+    INTO v_nom, v_prenom;
+
+    IF fin
+    THEN -- Plus besoin de "= 1"
+      LEAVE loop_curseur;
+    END IF;
+
+    SELECT CONCAT(v_prenom, ' ', v_nom) AS 'Client';
+  END LOOP;
+
+  CLOSE curs_clients;
+  END$$
+
+CREATE DEFINER =`root`@`localhost` PROCEDURE `test_iterate`() BEGIN
+  DECLARE v_i INT DEFAULT 0;
+
+  boucle_while: WHILE v_i < 3 DO
+    SET v_i = v_i + 1;
+    SELECT
+      v_i,
+      'Avant IF' AS message;
+
+    IF v_i = 2
+    THEN
+      ITERATE boucle_while;
+    END IF;
 
     SELECT
-      i,
-      v_stop;
-
-    SELECT *
-    FROM aazt.b1;
-
-    SELECT *
-    FROM aazt.b2;
-
-  END;
-
-CALL boucle_b1();
-
-create table xuB2
-  SELECT *
-  FROM b2;
+      v_i,
+      'Après IF' AS message;
+    -- Ne sera pas exécuté pour v_i = 2
+  END WHILE;
+  END$$
+*/
