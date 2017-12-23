e91e7a989e96c1096248e0a30dc9d40c6f6c2fa5
COTE <GrCOTE7@GMail.com>
2017-12-23 15:00:05 +0100

    Add police manuscrite
    Et produit remarquable

--- tuto/cours/uuuform2.php
+++ tuto/cours/uuuform2.php
@@ -1,19 +1,35 @@
-<p>
-  Texte avant le formulaire
-</p>
+<div class="gino">Texte avant le formulaire</div>
 
 <form method="POST" action="./tuto/cours/uuutraitement2.php">
 
   <fieldset>
-   
+
     <!-- Titre du fieldset -->
     <legend>&nbsp; Vos coordonnées &nbsp;</legend>
-    
+
     <label for="prenom">Quel est votre prénom ?<br></label>
     <input type="text" name="prenom" id="prenom"/><br>
-   
-    <input type="submit" value="Envoyer" id="submit"/><br>   
-    
-	</fieldset>
-		
+
+    <input type="submit" value="Envoyer" id="submit"/><br>
+
+  </fieldset>
+
 </form>
+
+<hr>
+
+<?php
+
+$a=5;
+$b=7;
+
+//$resultat = ($a+$b) * ($a+$b);
+
+$resultat = pow ( $a+$b , 2);
+
+$resultat2 = pow ( $a , 2) + 2*$a*$b + pow ( $b , 2);
+
+echo $resultat. ' '. $resultat2;
+
+
+
