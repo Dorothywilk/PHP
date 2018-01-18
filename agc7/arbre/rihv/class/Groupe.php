0b767d35a066781fcaedcebc82e7336b9ebd99f3
COTE <GrCOTE7@GMail.com>
2018-01-18 21:03:19 +0100

    Ajout tri de la collection // critère bg

--- agc7/arbre/rihv/class/Groupe.php
+++ agc7/arbre/rihv/class/Groupe.php
@@ -83,7 +83,64 @@ class Groupe
 //      echo '<li>' . $k . ' :  ' . $m->nom . '</li>';
       echo "<li>$k :  $m->nom ($m->bg, $m->bd | Parr: $m->parr |Prof:
 $m->pf | Type: $m->t)  </li>";
-    echo '</ul><hr>';
+    echo '</ul>';
+  }
+
+
+  public function affVueHierarchique()
+  {
+    $v = '<section class="basic-style">
+
+  <div class="hv-container">
+    <div class="hv-wrapper">
+
+      ';
+
+    foreach ( $this->membres as $i => $xu ) {
+      // $v = ''; // Affichage rien pdt tri
+
+      $xu->ido = $i; // Id d'origine
+
+      usort( $this->membres, __NAMESPACE__ . '\Groupe::compare_bg_membres' );
+//      echo $xu->nom . '<br>';
+    }
+//    echo '<hr>';
+
+
+    foreach ( $this->membres as $i => $xu ) {
+      $xu->id = $i;
+//      echo $xu->nom . ' id =  ' . $xu->id . '<br>';
+// Affichage de la carte positionnée de l'élément
+      $v .= $this->affCardXu( $xu );
+    }
+
+
+    $v .= '  </div>
+        </div>
+      </div>
+
+  </div>
+</section>
+';
+
+    echo $v;
+
+  }
+
+  /**
+   * Fonction pour trier les membres de la classe Groupe
+   * en fonction de leur borne gauche
+   *
+   * @param $a Membre A
+   * @param $b Membre B
+   * @return int
+   */
+  static public function compare_bg_membres( $a, $b )
+  {
+    if ( $a->bg === $b->bg ) {
+      return 0;
+    }
+    return ( $a->bg > $b->bg ) ? +1 : -1;
   }
 
 
@@ -109,7 +166,7 @@ $m->pf | Type: $m->t)  </li>";
       ';
 
     foreach ( $this->membres as $i => $xu )
-      $v .= $this->affCardXu( $i );
+      $v .= $this->affCardXu( $xu );
 
     $v .= '  </div>
         </div>
@@ -122,9 +179,11 @@ $m->pf | Type: $m->t)  </li>";
     return $v;
   }
 
-  public function affCardXu( $id )
+  public function affCardXu( $xu )
   {
-    $xu = $this->membres[ $id ];
+//    $xu = $this->membres[ $id ];
+
+//    vd( $xu );
 
     $node = $finnode = $finnodeprec = '';
     if ( $xu->t === 'p' ) {
@@ -140,7 +199,7 @@ $m->pf | Type: $m->t)  </li>";
       $type = 'child';
       $coulItem = 'blueLi';
     }
-    $ssnode = ( $id > 0 ) ? '<div class="hv-item-child">' : '';
+    $ssnode = ( $xu->id > 0 ) ? '<div class="hv-item-child">' : '';
     $vt = '';
 //    $vt = ' ('.serialize( $this->bds ).') '; // valeur test
 //    $vt =( isset($this->bds[ 0 ])) ?($this->bds)) :'non';
@@ -148,13 +207,13 @@ $m->pf | Type: $m->t)  </li>";
     if ( array_key_exists( 0, $this->bds ) && $xu->bd + 1 === $this->bds[ 0 ] ) {
 //      $vt = '*';
       array_shift( $this->bds );
-      $finnodeprec = '<!-- ID = ' . $id . ' -->' . '
+      $finnodeprec = '<!-- ID = ' . $xu->id . ' -->' . '
             </div>
         </div>
     </div>
     ';
-      if ( array_key_exists( $id + 1, $this->membres ) &&
-        $this->membres[ $id + 1 ]->bg > $this->bds[ 0 ]
+      if ( array_key_exists( $xu->id + 1, $this->membres ) &&
+        $this->membres[ $xu->id + 1 ]->bg > $this->bds[ 0 ]
       ) {
         $finnodeprec .= '</div>
         </div>
@@ -162,8 +221,7 @@ $m->pf | Type: $m->t)  </li>";
       }
     }
 
-
-    $aff = $id . ': ' . $this->membres[ $id ]->nom . $vt . '<br>' . ( $xu->parr ? ' < '
+    $aff = $xu->ido . ': ' . $this->membres[ $xu->id ]->nom . $vt . '<br>' . ( $xu->parr ? ' < '
         . $xu->parr : '' ) . '<br>(' .
       $xu->bg .
       ',' .
--- agc7/arbre/rihv/index.php
+++ agc7/arbre/rihv/index.php
@@ -13,47 +13,21 @@ include 'assets/functions/gc7.php';
 // Tentative de renommer un fondateur rejettée par le fait que la classe Groupe est un singleton
 // $gr = new Groupe ( 'Aadminli2');
 $gr = new Groupe ( 'Doro' );
+//$gr = new Groupe ( 'Doro2' );
 
 $gr->add( 'Jona', 0 ); // 0 = N° du membre parrain
-$gr->add( 'Mimi', 0 ); // 0 = N° du membre parrain
-$gr->add( 'Rom1', 0 ); // 0 = N° du membre parrain
-$gr->add( 'Jade', 0 ); // 0 = N° du membre parrain
-$gr->add( 'Fanny', 1 ); // 0 = N° du membre parrain
-$gr->add( 'Jeny', 4 ); // 0 = N° du membre parrain
-//$gr->add( 'Bug', 7 ); // 0 = N° du membre parrain
-$gr->affListeMembres();
+$gr->add( 'Mimi', 0 );
+$gr->add( 'Rom1', 0 );
+$gr->add( 'Jade', 0 );
+$gr->add( 'Fanny', 1 );
+$gr->add( 'Jeny', 4 );
+$gr->add( 'Félicien', 3 );
+$gr->add( 'Nico', 3 );
+$gr->add( 'Lionel', 0 );
+//$gr->add( 'Bug', 7 ); // 0 = N° du membre parrain // Todoli gérer controle
 
+// Affiche la liste des membres
+//$gr->affListeMembres();
 
-//
-//$famille = [
-//  [ 'bg' => 1, 'bd' => 2, 'nom' => 'Aadminli', 'parr' => null, 'prof' => 0 ],
-//];
-//
-//$famille = Ri::addChild( 'Doro', 1, $famille ); // 1 est la BG d'Aadminli
-//$famille = Ri::addChild( 'Mimi', 1, $famille );
-//$famille = Ri::addChild( 'Jona', 4, $famille );
-//$famille = Ri::addChild( 'Fanny', 3, $famille ); // < Fanny => Bug
-//$famille = Ri::addChild( 'Minnie', 3, $famille );
-//$famille = Ri::addChild( 'Jade', 4, $famille );
-//$famille = Ri::addChild( 'Chiva', 5, $famille );
-//$famille = Ri::addChild( 'Fanny', 4, $famille );
-//$famille = Ri::addChild( 'Ulysse', 3, $famille );
-//$famille = Ri::addChild( 'Nono', 9, $famille );
-////$famille = Ri::addChild( 'Alice', 2, $famille );
-
-//sort( $famille );
-
-//vd($famille);
-
-
-//echo '<pre>';
-//var_dump( $rsx );
-//echo '</pre>';
-
-//echo $rsx->nbr() . ' membres<hr>';
-
-//echo $rsx->affNomXu( 2 );
-//echo $rsx->affCardXu( 0 );
-
-//$rsx = new Rsx( $famille );
-//echo $rsx->affVueHV();
+// Affiche une vue hierarchique du Groupe
+$gr->affVueHierarchique();
